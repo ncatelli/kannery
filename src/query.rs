ed3de5dfe9c63358845512ba975827866f4bc0a4
Nate Catelli <ncatelli@packetfire.org>
2023-03-07 05:16:32 +0000

    docstring

--- src/query.rs
+++ src/query.rs
@@ -281,6 +281,35 @@ where
         }
     }
 
+    /// Evaluate a query, returning the results.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use kannery::*;
+    /// use kannery::query::*;;
+    ///
+    /// let query = QueryBuilder::default()
+    ///     .with_var('a')
+    ///     .with_var('b')
+    ///     .with_term(Term::value(1_u8))
+    ///     .build(|((a, b), one)| {
+    ///         conjunction(
+    ///             conjunction(equal(b.clone(), one.clone()), equal(Term::value(1), one)),
+    ///             equal(a, b),
+    ///         )
+    ///     });
+    ///
+    /// let result = query.run();
+    /// let a_values = result.values_of('a');
+    /// let b_values = result.values_of('b');
+    ///
+    /// // assert all values of a == 1.
+    /// assert!(a_values.into_iter().all(|val| val.as_ref() == &1_u8));
+    ///
+    /// // assert all values of b == 1.
+    /// assert!(b_values.into_iter().all(|val| val.as_ref() == &1_u8))
+    /// ```
     pub fn run(self) -> QueryResult<T> {
         let state = self.state;
         let goal = self.goal;
